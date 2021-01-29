6be992ca95d6467884d3eb5ad297f26cd5b59a76
Max Savitsky <maxsavitsky2015@gmail.com>
2021-01-29 18:39:21 +0300

    new backspace image

--- app/src/main/java/com/maxsavteam/newmcalc2/fragments/fragment1/Fragment1.java
+++ app/src/main/java/com/maxsavteam/newmcalc2/fragments/fragment1/Fragment1.java
@@ -1,13 +1,10 @@
 package com.maxsavteam.newmcalc2.fragments.fragment1;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.text.Html;
 import android.view.Display;
 import android.view.LayoutInflater;
@@ -15,8 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TableLayout;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,12 +22,12 @@ import androidx.fragment.app.Fragment;
 import com.maxsavteam.newmcalc2.R;
 
 public class Fragment1 extends Fragment {
-	private Context c;
-	private View.OnLongClickListener[] longClickListeners;
+	private final Context mContext;
+	private final View.OnLongClickListener[] mLongClickListeners;
 
 	public Fragment1(InitializationObject initializationObject) {
-		c = initializationObject.getContext();
-		this.longClickListeners = initializationObject.getLongClickListeners();
+		mContext = initializationObject.getContext();
+		this.mLongClickListeners = initializationObject.getLongClickListeners();
 	}
 
 	@Override
@@ -42,8 +39,9 @@ public class Fragment1 extends Fragment {
 
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		view = inflater.inflate( R.layout.fragment_1, container, false );
-		String[] arr = c.getResources().getStringArray( R.array.additional_chars );
+		//view = inflater.inflate( R.layout.fragment_1, container, false );
+		view = LayoutInflater.from( mContext ).inflate( R.layout.fragment_1, container, false );
+		String[] arr = mContext.getResources().getStringArray( R.array.additional_chars );
 		int[] btnIds = {
 				R.id.btn7,
 				R.id.btn8,
@@ -66,7 +64,7 @@ public class Fragment1 extends Fragment {
 		for (int ii = 0; ii < btnIds.length; ii++) {
 			Button btn = view.findViewById( btnIds[ ii ] );
 			btn.setTransformationMethod( null );
-			btn.setOnLongClickListener( longClickListeners[ 0 ] );
+			btn.setOnLongClickListener( mLongClickListeners[ 0 ] );
 			String num = btn.getText().toString().substring( 0, 1 );
 			if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ) {
 				btn.setText( Html.fromHtml( num + "<sup><small><small><small>" + arr[ ii ] + "</small></small></small></sup>", Html.FROM_HTML_MODE_COMPACT ) );
@@ -75,11 +73,13 @@ public class Fragment1 extends Fragment {
 			}
 		}
 		Button b = view.findViewById( R.id.btnCalc );
-		b.setOnLongClickListener( longClickListeners[ 1 ] );
-		b = view.findViewById( R.id.btnDelete );
-		b.setOnLongClickListener( longClickListeners[ 2 ] );
-		WindowManager windowManager = (WindowManager) c.getSystemService( Context.WINDOW_SERVICE );
-		if ( c.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE && windowManager != null ) {
+		b.setOnLongClickListener( mLongClickListeners[ 1 ] );
+
+		ImageButton imageButton = view.findViewById( R.id.btnDelete );
+		imageButton.setOnLongClickListener( mLongClickListeners[ 2 ] );
+
+		WindowManager windowManager = (WindowManager) mContext.getSystemService( Context.WINDOW_SERVICE );
+		if ( mContext.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE && windowManager != null ) {
 			TableLayout tbl = view.findViewById( R.id.tbl );
 			ViewGroup.LayoutParams tblLayoutParams = tbl.getLayoutParams();
 			Display display = windowManager.getDefaultDisplay();
