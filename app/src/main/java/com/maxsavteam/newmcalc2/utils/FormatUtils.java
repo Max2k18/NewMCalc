13347a52fda5f1a811b914d661af3b7baf8e9d6e
Max Savitsky <maxsavitsky2015@gmail.com>
2022-06-09 10:55:10 +0300

    refactor: refactor FormatUtils to support UTF-16 symbols

--- app/src/main/java/com/maxsavteam/newmcalc2/utils/FormatUtils.java
+++ app/src/main/java/com/maxsavteam/newmcalc2/utils/FormatUtils.java
@@ -10,111 +10,126 @@ public class FormatUtils {
 
 	private static final String TAG = Main2Activity.TAG + " FormatUtils";
 
-	public static String formatText(String text, DecimalFormatSymbols formatSymbols){
+	public static String formatText(String text, DecimalFormatSymbols formatSymbols) {
 		StringBuilder formatted = new StringBuilder();
 		StringBuilder number = new StringBuilder();
-		for(char c : text.toCharArray()){
-			if(c >= '0' && c <= '9' || c == formatSymbols.getDecimalSeparator())
-				number.append( c );
-			else if(c != formatSymbols.getGroupingSeparator()){
-				if(number.length() > 0) {
+		for (int i = 0; i < text.length(); i++) {
+			// we should use substrings because of UTF-16 symbols (such as degree sign)
+			// UTF-16 characters are split into two separate characters
+			String character = text.substring( i, i + 1 );
+			if ( isDigit( character ) || character.equals( String.valueOf( formatSymbols.getDecimalSeparator() ) ) ) {
+				number.append( character );
+			} else if ( !character.equals( String.valueOf( formatSymbols.getGroupingSeparator() ) ) ) {
+				if ( number.length() > 0 ) {
 					formatted
 							.append( formatNumber( number.toString(), formatSymbols ) );
 					number.setLength( 0 );
 				}
-				formatted
-						.append( c );
+				formatted.append( character );
 			}
 		}
-		if(number.length() > 0)
+		if ( number.length() > 0 ) {
 			formatted
 					.append( formatNumber( number.toString(), formatSymbols ) );
+		}
 		return formatted.toString();
 	}
 
-	public static String formatNumber(String number, DecimalFormatSymbols formatSymbols, DecimalFormatSymbols sourceSymbols){
+	public static String formatNumber(String number, DecimalFormatSymbols formatSymbols, DecimalFormatSymbols sourceSymbols) {
 		int dotPos = number.indexOf( sourceSymbols.getDecimalSeparator() );
 		StringBuilder sb = new StringBuilder();
-		if(dotPos != -1){
-			for(int i = number.length() - 1; i > dotPos; i--){
+		if ( dotPos != -1 ) {
+			for (int i = number.length() - 1; i > dotPos; i--) {
 				char c = number.charAt( i );
-				if(c >= '0' && c <= '9')
+				if ( c >= '0' && c <= '9' ) {
 					sb.append( c );
+				}
 			}
 			sb.append( formatSymbols.getDecimalSeparator() );
 		}
 		int insertedCount = 0;
-		if(dotPos == -1)
+		if ( dotPos == -1 ) {
 			dotPos = number.length();
-		for(int i = dotPos - 1; i >= 0; i--){
+		}
+		for (int i = dotPos - 1; i >= 0; i--) {
 			char c = number.charAt( i );
-			if(c >= '0' && c <= '9'){
+			if ( c >= '0' && c <= '9' ) {
 				sb.append( c );
 				insertedCount++;
-				if(insertedCount % 3 == 0)
+				if ( insertedCount % 3 == 0 ) {
 					sb.append( formatSymbols.getGroupingSeparator() );
+				}
 			}
 		}
-		if(sb.length() > 0 && sb.charAt( sb.length() - 1 ) == formatSymbols.getGroupingSeparator())
+		if ( sb.length() > 0 && sb.charAt( sb.length() - 1 ) == formatSymbols.getGroupingSeparator() ) {
 			sb.deleteCharAt( sb.length() - 1 );
+		}
 		return sb
 				.reverse()
 				.toString();
 	}
 
-	public static String formatNumber(String number, DecimalFormatSymbols symbols){
+	public static String formatNumber(String number, DecimalFormatSymbols symbols) {
 		return formatNumber( number, symbols, symbols );
 	}
 
-	public static String normalizeNumbersInExample(String ex, DecimalFormat decimalFormat){
+	public static String normalizeNumbersInExample(String ex, DecimalFormat decimalFormat) {
 		decimalFormat.setParseBigDecimal( true );
 		DecimalFormatSymbols symbols = decimalFormat.getDecimalFormatSymbols();
 		StringBuilder formatted = new StringBuilder();
 		StringBuilder number = new StringBuilder();
-		for(char c : ex.toCharArray()){
-			if(Character.isDigit( c ))
-				number
-						.append( c );
-			else if(c == symbols.getDecimalSeparator()){
+		for (int i = 0; i < ex.length(); i++) {
+			// we should use substrings because of UTF-16 symbols (such as degree sign)
+			// UTF-16 characters are split into two separate characters
+			String character = ex.substring( i, i + 1 );
+			if ( isDigit( character ) ) {
+				number.append( character );
+			} else if ( character.equals( String.valueOf( symbols.getDecimalSeparator() ) ) ) {
 				number.append( "." );
-			}
-			else if(c != symbols.getGroupingSeparator()){
-				if(number.length() > 0) {
+			} else if ( !character.equals( String.valueOf( symbols.getGroupingSeparator() ) ) ) {
+				if ( number.length() > 0 ) {
 					formatted
 							.append( number );
 					number.setLength( 0 );
 				}
 				formatted
-						.append( c );
+						.append( character );
 			}
 		}
-		if(number.length() > 0)
-			formatted
-					.append( number );
+		if ( number.length() > 0 ) {
+			formatted.append( number );
+		}
 		return formatted.toString();
 	}
 
-	public static String formatNumbersInExpression(String ex, DecimalFormat decimalFormat){
+	public static String formatNumbersInExpression(String ex, DecimalFormat decimalFormat) {
 		StringBuilder formatted = new StringBuilder();
 		StringBuilder number = new StringBuilder();
-		for(char c : ex.toCharArray()){
-			if(Character.isDigit( c ) || c == '.'){
-				number
-						.append( c );
-			}else{
-				if(number.length() > 0) {
-					formatted
-							.append( decimalFormat.format( new BigDecimal( number.toString() ) ) );
+		for (int i = 0; i < ex.length(); i++) {
+			// we should use substrings because of UTF-16 symbols (such as degree sign)
+			// UTF-16 characters are split into two separate characters
+			String character = ex.substring( i, i + 1 );
+			if ( isDigit( character ) || ".".equals( character ) ) {
+				number.append( character );
+			} else {
+				if ( number.length() > 0 ) {
+					formatted.append( decimalFormat.format( new BigDecimal( number.toString() ) ) );
 					number.setLength( 0 );
 				}
-				formatted
-						.append( c );
+				formatted.append( character );
 			}
 		}
-		if(number.length() > 0)
-			formatted
-					.append( decimalFormat.format( new BigDecimal( number.toString() ) ) );
+		if ( number.length() > 0 ) {
+			formatted.append( decimalFormat.format( new BigDecimal( number.toString() ) ) );
+		}
 		return formatted.toString();
+	}
+
+	private static boolean isDigit(String character) {
+		if ( character.length() > 1 ) {
+			return false;
+		}
+		return Character.isDigit( character.codePointAt( 0 ) );
 	}
 
 }
